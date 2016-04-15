939c817e246b5391a0c21b968d9094ad25b75f1c
test@catlight.io <test@catlight.io>
2016-04-14 19:47:57 -0700

    Added nunit tests

---
+++ NUnit-CS-Success/TestClass1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace NUnit_CS_Success
+{
+    [TestFixture]
+    public class TestClass1
+    {
+        [Test]
+        public void Test1()
+        {
+        }
+
+        [Test]
+        public void Test2()
+        {
+        }
+    }
+}
